4b2649a33c734ac948dcb2ee6244ebae74bbea72
imad <j166a775433@gmail.com>
2026-09-15 18:00:25 +0100

    Update main.tf with user data

--- 01-lab01/main.tf
+++ 01-lab01/main.tf
@@ -2,7 +2,11 @@ resource "aws_instance" "wordpress" {
   ami           = "ami-0bf5d5d5c92a17e24"
   instance_type = var.instance_type
 
-vpc_security_group_ids = [aws_security_group.wordpress.id]
+  vpc_security_group_ids = [aws_security_group.wordpress.id]
+
+  user_data                   = file("${path.module}/cloud-init.sh")
+  user_data_replace_on_change = true
+
   tags = {
     Name = "wordpress"
   }
