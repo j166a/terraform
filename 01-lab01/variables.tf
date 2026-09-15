f0a5114e9d40de8fc73735ad69055b09de3898f1
imad <j166a775433@gmail.com>
2026-09-15 20:04:09 +0100

    Add ssh_cidr

--- 01-lab01/variables.tf
+++ 01-lab01/variables.tf
@@ -9,3 +9,14 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-2"
 }
+
+variable "key_name" {
+  description = "Name of the SSH key pair to use for the instance"
+  type        = string
+  default     = "ec2-demo-vpc"
+}
+
+variable "ssh_cidr" {
+  description = "CIDR block for SSH access"
+  type        = string
+}
