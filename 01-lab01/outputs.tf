fc70987ac9ce6a5298292866d7657bb90106968a
imad <j166a775433@gmail.com>
2026-09-15 18:02:04 +0100

    Format outputs.tf

--- 01-lab01/outputs.tf
+++ 01-lab01/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-   value = aws_instance.wordpress.id
- }
+  value = aws_instance.wordpress.id
+}
 
- output "instance_public_ip" {
-   value = aws_instance.wordpress.public_ip
- }
+output "instance_public_ip" {
+  value = aws_instance.wordpress.public_ip
+}
