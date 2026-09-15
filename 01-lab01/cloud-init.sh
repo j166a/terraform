7149396506956ba10b9cffba3933dbf5eb4072be
imad <j166a775433@gmail.com>
2026-09-15 18:00:05 +0100

    Fix EOF with single quotations

--- 01-lab01/cloud-init.sh
+++ 01-lab01/cloud-init.sh
@@ -4,7 +4,7 @@ set -eux
 yum update -y
 yum install -y httpd php php-mysqlnd php-fpm
 
-cat > /var/www/html/index.php <<EOF
+cat > /var/www/html/index.php <<'EOF'
 <?php
 echo "<h1>WordPress EC2 instance ready</h1>";
 echo "<p>PHP is working!</p>";
