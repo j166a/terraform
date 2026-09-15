b589522175109eaec9f92133f8fb45b491c799e4
imad <j166a775433@gmail.com>
2026-09-15 17:33:05 +0100

    Add cloud init

---
+++ 01-lab01/cloud-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eux
+
+yum update -y
+yum install -y httpd php php-mysqlnd php-fpm
+
+cat > /var/www/html/index.php <<EOF
+<?php
+echo "<h1>WordPress EC2 instance ready</h1>";
+echo "<p>PHP is working!</p>";
+?>
+EOF
+
+systemctl enable --now php-fpm
+systemctl enable --now httpd
