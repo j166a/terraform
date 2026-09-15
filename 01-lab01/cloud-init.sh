058cb3f2abb2249802e99673eaf52d0677989150
imad <j166a775433@gmail.com>
2026-09-15 18:16:41 +0100

    Add wordpress db

--- 01-lab01/cloud-init.sh
+++ 01-lab01/cloud-init.sh
@@ -8,6 +8,17 @@ systemctl enable --now mariadb
 systemctl enable --now php-fpm
 systemctl enable --now httpd
 
+mysql <<'SQL'
+CREATE DATABASE wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE USER 'wordpress'@'localhost'
+IDENTIFIED BY 'wordpress-password';
+
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost';
+
+FLUSH PRIVILEGES;
+SQL
+
 cat > /var/www/html/index.php <<'EOF'
 <?php
 echo "<h1>WordPress EC2 instance ready</h1>";
