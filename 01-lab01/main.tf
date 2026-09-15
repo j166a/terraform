b145e957eb9324669c6aa82e0cd579dcf7aba53a
imad <j166a775433@gmail.com>
2026-09-15 17:12:25 +0100

    Add wordpress_sg

--- 01-lab01/main.tf
+++ 01-lab01/main.tf
@@ -7,3 +7,21 @@ resource "aws_instance" "wordpress" {
   }
 }
 
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress_sg"
+  description = "Allow HTTP traffic to WordPress instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
